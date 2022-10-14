d2ecce03c7cfccde0cf43c8b96319fd69fb881df
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-14 15:50:00 -0300

    feat: :lipstick: Adição de logo na página Home

--- src/screens/Home/Index.tsx
+++ src/screens/Home/Index.tsx
@@ -1,7 +1,13 @@
-import { View } from "react-native";
+import { Image, View } from "react-native";
 
 import { styles } from "./styles";
 
+import logo from "@assets/logo-nlw-esports.png";
+
 export function Home() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<Image source={logo} style={styles.logo} />
+		</View>
+	);
 }
