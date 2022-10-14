5c9068b3c37bde7572efe6d99e2279a425da31ba
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-14 19:07:27 -0300

    feat: :lipstick: Componente Heading

--- src/screens/Home/Index.tsx
+++ src/screens/Home/Index.tsx
@@ -3,11 +3,17 @@ import { Image, View } from "react-native";
 import { styles } from "./styles";
 
 import logo from "@assets/logo-nlw-esports.png";
+import { Heading } from "@components/Heading";
 
 export function Home() {
 	return (
 		<View style={styles.container}>
 			<Image source={logo} style={styles.logo} />
+
+			<Heading
+				title="Encontre seu duo!"
+				subTitle="Selecione o game que deseja jogar..."
+			/>
 		</View>
 	);
 }
