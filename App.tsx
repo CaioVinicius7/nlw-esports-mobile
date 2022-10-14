40dd13229b3d15045f6c75a47c0a23a9c036ebec
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-14 15:35:19 -0300

    feat: :lipstick: Adição de fonte personalizada e componente Loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
-import { StatusBar, View } from "react-native";
+import { StatusBar } from "react-native";
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	Inter_900Black
+} from "@expo-google-fonts/inter";
 
 import { Background } from "@components/Background";
+import { Home } from "@screens/Home/Index";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_600SemiBold,
+		Inter_700Bold,
+		Inter_900Black
+	});
+
 	return (
 		<Background>
 			<StatusBar
@@ -10,7 +26,8 @@ export default function App() {
 				backgroundColor="transparent"
 				translucent
 			/>
-			<View></View>
+
+			{fontsLoaded ? <Home></Home> : <Loading />}
 		</Background>
 	);
 }
