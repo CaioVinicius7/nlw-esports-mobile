334a3b9a41045807dd117714fede8d873b057702
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-24 15:03:32 -0300

    feat: :lipstick: Header da tela Game

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,21 +1,44 @@
+import { Image, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useRoute } from "@react-navigation/native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { Entypo } from "@expo/vector-icons";
 
+import { THEME } from "@theme/index";
 import { Background } from "@components/Background";
+import { Heading } from "@components/Heading";
 
 import { styles } from "./styles";
 import { GameParams } from "../../@types/navigation";
 
+import logo from "../../assets/logo-nlw-esports.png";
+
 export function Game() {
 	const route = useRoute();
+	const navigation = useNavigation();
 
 	const game = route.params as GameParams;
 
-	console.log(game);
+	function handleGoBack() {
+		navigation.goBack();
+	}
 
 	return (
 		<Background>
-			<SafeAreaView style={styles.container}></SafeAreaView>
+			<SafeAreaView style={styles.container}>
+				<View style={styles.header}>
+					<TouchableOpacity onPress={handleGoBack}>
+						<Entypo
+							name="chevron-thin-left"
+							color={THEME.COLORS.CAPTION_300}
+							size={20}
+						/>
+					</TouchableOpacity>
+
+					<Image source={logo} style={styles.logo} />
+
+					<View style={styles.right} />
+				</View>
+			</SafeAreaView>
 		</Background>
 	);
 }
